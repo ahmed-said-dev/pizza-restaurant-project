46a54237c2fa3b1a4b0c7d1e4203acb85d063b61
ahmed-said-dev <105396982+ahmed-said-dev@users.noreply.github.com>
2022-11-07 20:25:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,11 @@
-// let left = document.getElementsByClassName("bi-caret-left-fill")[0];
-// let right = document.getElementsByClassName("bi-caret-right-fill")[0];
-// let cards = document.getElementsByClassName("cards")[0];
-
-// left.addEventListener("click",() => {
-//     cards.scrollLeft = -140 ;
-// })
-// right.addEventListener("click",() => {
-//     cards.scrollLeft = +140 ;
-// })
-
-// let poster = document.getElementById("poster");
-// let title = document.getElementById("title")
-// let price = document.getElementById("price_cont")
-
-// Array.from(document.getElementsByClassName("card")).forEach((ele,i)=>{
-//     console.log(ele);
-// })
-
 let right = document.getElementsByClassName("bi-caret-right-fill")[0];
 let left = document.getElementsByClassName("bi-caret-left-fill")[0];
 let cards = document.getElementsByClassName("cards")[0];
-
 left.addEventListener("click",()=>{cards.scrollLeft = -140;});
-right.addEventListener("click",()=> {cards.scrollLeft = +140;} );
-
+right.addEventListener("click",()=> {cards.scrollLeft = +700;} );
 let poster = document.getElementById("poster");
 let title = document.getElementById("title")
 let price =document.getElementById("price_count")
-
-
-
-
 Array.from(document.getElementsByClassName("card")).forEach((ele,i)=>{
     ele.addEventListener("click",()=>{ 
         poster.src = ele.getElementsByTagName("img")[0].src;
