5f33bf89bcfc40cd5bf16b1b74f829ecf29d6fb6
ahmed-said-dev <105396982+ahmed-said-dev@users.noreply.github.com>
2022-11-07 17:33:55 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+// let left = document.getElementsByClassName("bi-caret-left-fill")[0];
+// let right = document.getElementsByClassName("bi-caret-right-fill")[0];
+// let cards = document.getElementsByClassName("cards")[0];
+
+// left.addEventListener("click",() => {
+//     cards.scrollLeft = -140 ;
+// })
+// right.addEventListener("click",() => {
+//     cards.scrollLeft = +140 ;
+// })
+
+// let poster = document.getElementById("poster");
+// let title = document.getElementById("title")
+// let price = document.getElementById("price_cont")
+
+// Array.from(document.getElementsByClassName("card")).forEach((ele,i)=>{
+//     console.log(ele);
+// })
+
+let right = document.getElementsByClassName("bi-caret-right-fill")[0];
+let left = document.getElementsByClassName("bi-caret-left-fill")[0];
+let cards = document.getElementsByClassName("cards")[0];
+
+left.addEventListener("click",()=>{cards.scrollLeft = -140;});
+right.addEventListener("click",()=> {cards.scrollLeft = +140;} );
+
+let poster = document.getElementById("poster");
+let title = document.getElementById("title")
+let price =document.getElementById("price_count")
+
+
+
+
+Array.from(document.getElementsByClassName("card")).forEach((ele,i)=>{
+    ele.addEventListener("click",()=>{ 
+        poster.src = ele.getElementsByTagName("img")[0].src;
+        title.textContent = ele.getElementsByTagName("h5")[0].textContent;
+        price.textContent = ele.getElementsByTagName("h4")[0].textContent;
+    } )
+})
