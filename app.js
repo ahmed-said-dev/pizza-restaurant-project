2a26bd8795bf24a51928a1920040e90e010eb4d2
ahmed-said-dev <105396982+ahmed-said-dev@users.noreply.github.com>
2022-11-07 20:34:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let right = document.getElementsByClassName("bi-caret-right-fill")[0];
 let left = document.getElementsByClassName("bi-caret-left-fill")[0];
 let cards = document.getElementsByClassName("cards")[0];
 left.addEventListener("click",()=>{cards.scrollLeft = -140;});
-right.addEventListener("click",()=> {cards.scrollLeft = +700;} );
+right.addEventListener("click",()=> {cards.scrollLeft = +750;} );
 let poster = document.getElementById("poster");
 let title = document.getElementById("title")
 let price =document.getElementById("price_count")
